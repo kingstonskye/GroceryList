5d7659ef9a36e2160e825f6b8d0124211b4e3e3a
kingstonskye <125080702+kingstonskye@users.noreply.github.com>
2023-08-08 16:06:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const groceries = document.getElementsByClassName("groceries")[0];
 const pencil = document.getElementById("pencil");
 const allItems = document.getElementById("allItems");
-const i = document.getElementById("userInput");
+const userInput = document.getElementById("userInput");
 
 pencil.addEventListener("click", function(){
     allItems.innerHTML = "";
